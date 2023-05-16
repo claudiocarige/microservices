764f0a77f724cfe16d410815b845b215f981d47a
ClaudioCarige <ccarige@gmail.com>
2023-05-15 22:26:22 -0300

    Created Domain Client and insert anotation in Main Application

--- microclient/src/main/java/com/claudiocarige/microclient/MicroclientApplication.java
+++ microclient/src/main/java/com/claudiocarige/microclient/MicroclientApplication.java
@@ -2,8 +2,10 @@ package com.claudiocarige.microclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MicroclientApplication {
 
 	public static void main(String[] args) {
--- microclient/src/main/java/com/claudiocarige/microclient/domain/Client.java
+++ microclient/src/main/java/com/claudiocarige/microclient/domain/Client.java
@@ -1,2 +1,28 @@
-package com.claudiocarige.microclient.domain;public class Client {
+package com.claudiocarige.microclient.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String cpf;
+    private String name;
+    private Integer idade;
+
+    public Client(String cpf, String name, Integer idade) {
+        this.cpf = cpf;
+        this.name = name;
+        this.idade = idade;
+    }
 }

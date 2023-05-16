ad86fcccef729a277a161a1ab80d119df514beb0
ClaudioCarige <ccarige@gmail.com>
2023-05-15 22:38:37 -0300

    Created ClientResource - Controller

--- microclient/src/main/java/com/claudiocarige/microclient/application/ClientResource.java
+++ microclient/src/main/java/com/claudiocarige/microclient/application/ClientResource.java
@@ -1,2 +1,17 @@
-package com.claudiocarige.microclient.application;public class ClientResource {
+package com.claudiocarige.microclient.application;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+    @GetMapping
+    public String status(){
+        return "ok";
+    }
+
+
 }
+

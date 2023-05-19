927f5448d23e9cc7e67f8beb13fd2f9dbdc00018
ClaudioCarige <ccarige@gmail.com>
2023-05-19 11:49:26 -0300

    Created CardPersonalRepresentation(DTO)

--- mscartoes/src/main/java/com/claudiocarige/mscartoes/application/representation/CardPersonalRepresentation.java
+++ mscartoes/src/main/java/com/claudiocarige/mscartoes/application/representation/CardPersonalRepresentation.java
@@ -1,2 +1,4 @@
-package com.claudiocarige.mscartoes.application.representation;public class CardPersonalRepresentation {
+package com.claudiocarige.mscartoes.application.representation;
+
+public class CardPersonalRepresentation {
 }

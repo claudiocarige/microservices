19d32e4d565dc558f3eb15d15cc3d0c5b0ef3a62
ClaudioCarige <ccarige@gmail.com>
2023-05-19 11:50:44 -0300

    Created Query FindByIncome

--- mscartoes/src/main/java/com/claudiocarige/mscartoes/infra/repository/CardPersonalRepository.java
+++ mscartoes/src/main/java/com/claudiocarige/mscartoes/infra/repository/CardPersonalRepository.java
@@ -3,5 +3,8 @@ package com.claudiocarige.mscartoes.infra.repository;
 import com.claudiocarige.mscartoes.domain.CardPersonal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CardPersonalRepository extends JpaRepository<CardPersonal, Long> {
+    List<CardPersonal> findByIncomeLessThanEqual(Long income);
 }

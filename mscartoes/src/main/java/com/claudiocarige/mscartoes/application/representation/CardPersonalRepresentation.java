0832dcbcbd58bb667796283ca93e51ca4bec8cf1
ClaudioCarige <ccarige@gmail.com>
2023-05-19 12:05:29 -0300

    Created Method Insert in CardPersonalResouce and modified CardPersonalRepresentation

--- mscartoes/src/main/java/com/claudiocarige/mscartoes/application/representation/CardPersonalRepresentation.java
+++ mscartoes/src/main/java/com/claudiocarige/mscartoes/application/representation/CardPersonalRepresentation.java
@@ -1,4 +1,20 @@
 package com.claudiocarige.mscartoes.application.representation;
 
+import com.claudiocarige.mscartoes.domain.CardPersonal;
+import com.claudiocarige.mscartoes.domain.enums.CardFlag;
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
 public class CardPersonalRepresentation {
+
+    private String name;
+    private CardFlag flag;
+    private BigDecimal income;
+    private BigDecimal basicLimit;
+
+    public CardPersonal toModel(){
+        return new CardPersonal(name, flag, income, basicLimit);
+    }
 }
--- mscartoes/src/main/java/com/claudiocarige/mscartoes/application/resources/CardsResource.java
+++ mscartoes/src/main/java/com/claudiocarige/mscartoes/application/resources/CardsResource.java
@@ -1,15 +1,32 @@
 package com.claudiocarige.mscartoes.application.resources;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.claudiocarige.mscartoes.application.representation.CardPersonalRepresentation;
+import com.claudiocarige.mscartoes.application.services.CardPersonalService;
+import com.claudiocarige.mscartoes.domain.CardPersonal;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/cards")
+@RequiredArgsConstructor
 public class CardsResource {
- 
+
+    private final CardPersonalService cardPersonalService;
     @GetMapping
     public String status(){
         return "Cartões ok";
+    }
+
+    @PostMapping
+    public ResponseEntity<CardPersonal> insert(@RequestBody CardPersonalRepresentation cardRepresentation){
+        CardPersonal cardpersonal = cardRepresentation.toModel();
+        cardPersonalService.insert(cardpersonal);
+       return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
